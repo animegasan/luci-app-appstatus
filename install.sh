964efd481f39f0398e796529177a564edc3e0c4b
Hilman Maulana <hilman0.0maulana@gmail.com>
2024-07-25 15:08:00 +0700

    luci-app-appstatus: Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+file_name="luci-app-appstatus_1.1_all.ipk"
+version="v1.1"
+
+echo "Updating OPKG"
+opkg update
+echo "Downloading $file_name"
+curl -LO "https://github.com/animegasan/luci-app-appstatus/releases/download/$version/$file_name"
+echo "Installing $file_name"
+opkg install $file_name
+echo "Process completed. $file_name has been installed."
